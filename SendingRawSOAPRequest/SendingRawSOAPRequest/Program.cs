237f00a1d161d4aaae2d081fa9f286f47dacc1cf
NightCatcher <todongho@gmail.com>
2019-07-22 12:41:04 +0700

    IVNI-26 update 1st

---
+++ SendingRawSOAPRequest/SendingRawSOAPRequest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Xml;
+
+namespace SendingRawSOAPRequest
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var xmlUltis = new XmlUltils();
+            xmlUltis.SearchFistSixty("styx", "60");
+            Console.ReadKey();
+        }
+    }
+}
