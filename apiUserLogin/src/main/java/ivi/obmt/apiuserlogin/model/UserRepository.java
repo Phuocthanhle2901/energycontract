df4ef1d7cb1964ba7558d1fdc3b358850c455465
Võ Đại Hiệp <hiep.daivo@infodation.vn>
2024-12-12 10:12:26 +0700

    API for user login

---
+++ apiUserLogin/src/main/java/ivi/obmt/apiuserlogin/model/UserRepository.java
@@ -0,0 +1,7 @@
+package ivi.obmt.apiuserlogin.model;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
