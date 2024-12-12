2b1ad3664f323c2086b20f0941021fcd1cf56ce9
Võ Đại Hiệp <hiep.daivo@infodation.vn>
2024-12-12 09:28:48 +0700

    API for user registration

---
+++ apiUserRegistration/src/test/java/ivi/obmt/apiuserregistration/ApiUserRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ivi.obmt.apiuserregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUserRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
