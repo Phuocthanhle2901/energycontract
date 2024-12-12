9e3e645bf193f241e4686c01df5cb3113ee33a2d
Võ Đại Hiệp <hiep.daivo@infodation.vn>
2024-12-12 10:51:07 +0700

    API to view available meeting rooms

---
+++ apiViewRooms/src/test/java/ivi/obmt/apiviewrooms/ApiViewRoomsApplicationTests.java
@@ -0,0 +1,13 @@
+package ivi.obmt.apiviewrooms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiViewRoomsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
