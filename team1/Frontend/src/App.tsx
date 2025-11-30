34de9712ec6aa1d5d3d3b94569601c4108443c0b
Phuocthanhle2901 <phuoc.thanhle@infodation.vn>
2025-11-30 10:36:32 +0700

    pull intern2025-team1 ve FE_team1_LeThanhPhuoc

--- team1/Frontend/src/App.tsx
+++ team1/Frontend/src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 import AppRoutes from "./routes/AppRouter";
 import "./assets/styles.css";
@@ -6,23 +5,3 @@ import "./assets/styles.css";
 export default function App() {
   return <AppRoutes />;
 }
-=======
-
-import {BrowserRouter,Routes, Route} from "react-router-dom";
-import ResellerPage from "./pages/ResellerPage.tsx";
-import { Toaster } from "sonner";
-function App() {
-  return ( 
-    <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/reseller" element={<ResellerPage />} />
-            </Routes>
-        </BrowserRouter>
-      <Toaster richColors position={"top-right"}></Toaster>
-    </>
-  )
-}
-
-export default App
->>>>>>> 0fee2a44146a9d4217a88864a7771d627690aca5
--- team1/Frontend/src/types/reseller.ts
+++ team1/Frontend/src/types/reseller.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 export interface Reseller {
   id: number;
   name: string;
   type: string;
 }
-=======
-export interface Reseller {
-    id: number;
-    name: string;
-    type: string;
-}
->>>>>>> 0fee2a44146a9d4217a88864a7771d627690aca5
