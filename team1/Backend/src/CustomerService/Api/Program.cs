4ce4b0301872494e8a9f840fc87812da78baae66
Huy Ngô <huyngo@Huys-MacBook-Air.local>
2025-12-03 09:22:06 +0700

    save commit

--- team1/Backend/src/CustomerService/Api/Program.cs
+++ team1/Backend/src/CustomerService/Api/Program.cs
@@ -71,7 +71,9 @@ builder.Services.AddSwaggerGen(c =>
         {
             policy.WithOrigins(
                     "http://localhost:5173",
-                    "http://127.0.0.1:5173"
+                    "http://localhost:5174",
+                    "http://127.0.0.1:5173",
+                    "http://127.0.0.1:5174"
                   )
                   .AllowAnyMethod()
                   .AllowAnyHeader()
